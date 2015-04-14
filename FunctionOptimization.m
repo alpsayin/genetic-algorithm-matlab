63fb4e7f92fca6f1acc6e46df4c9bb99396ab2d3
Alp Sayin <alpsayin@gmail.com>
2015-04-14 10:49:38 +0100

    divide code into sections, on/off switch for plotting, use new vectorized functions

--- FunctionOptimization.m
+++ FunctionOptimization.m
@@ -6,108 +6,142 @@
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% CLEAN-UP
 clear;close all;clc;
 tic
-populationSize = 100 ;
+
+%% PARAMETERS
+populationSize = 150 ;
 numberOfGenes = 40;
 crossoverProbability = 0.8 ;
-mutationProbability = 0.025;
-tournamentSelectionParameter = 0.75;
-variableRange = 3.0;
-numberOfGenerations = 100;
-fitness = zeros(populationSize, 1);
+mutationProbability = 0.125;
+tournamentSelectionParameter = 0.5;
+variableRange = 10.0;
+numberOfGenerations = 250;
 numberOfVariables = 2;
-tournamentSize = 2;
+tournamentSize = 10;
 numberOfReplications = 2;
+verbose = true;
+draw_plots = true;
+% UNLESS THE FITNESS FUNCTION IS REALLY DIFFICULT TO COMPUTE, IT'S FASTER
+% NOT TO USE PARALLEL COMPUTATION
+runparallel = false; 
 
 
-fitnessFigureHandle = figure;
-hold on;
-set(fitnessFigureHandle,'Position',[50,50,500,200]);
-set(fitnessFigureHandle,'DoubleBuffer','on');
-axis([1 numberOfGenerations -variableRange variableRange]);
-bestPlotHandle = plot(1:numberOfGenerations, zeros(1,numberOfGenerations));
-textHandle = text(30,2.6, sprintf('best: %4.3f', 0.0));
-hold off;
-drawnow;
+%% VARIABLES
+fitness = zeros(populationSize, 1);
 
-surfaceFigureHandle= figure;
-hold on;
-set(surfaceFigureHandle,'DoubleBuffer','on');
-delta=0.1;
-limit = fix(2*variableRange/delta)+1 ;
-[xValues, yValues] = meshgrid(-variableRange: delta:variableRange,-variableRange: delta:variableRange);
-zValues= zeros(limit,limit);
-for j = 1: limit
-    for k = 1: limit
-        zValues(j,k) = EvaluateIndividual([xValues(j,k) yValues(j,k)]);
-    end
-end
-surfl(xValues,yValues,zValues)
-colormap gray;
-shading interp;
-view ([-7 -9 10]);
-decodedPopulation = zeros(populationSize,numberOfVariables);
-populationPlotHandle= plot3(decodedPopulation(:,1),decodedPopulation(:,2),fitness(:),'kp');
-hold off;
-drawnow;
+%% PLOTTING SETUP
+if draw_plots
+    fitnessFigureHandle = figure;
+    hold on;
+    set(fitnessFigureHandle,'Position',[50,50,500,200]);
+    set(fitnessFigureHandle,'DoubleBuffer','on');
+    axis([1 numberOfGenerations -variableRange variableRange]);
+    bestPlotHandle = plot(1:numberOfGenerations, zeros(1,numberOfGenerations));
+    textHandle = text(30,2.6, sprintf('best: %4.3f', 0.0));
+    hold off;
+    drawnow;
 
-population = InitializePopulation(populationSize, numberOfGenes) ;
-
-for iGeneration = 1: numberOfGenerations
-    
-    maximumFitness = 0.0;
-    for i = 1: populationSize
-        chromosome = population(i,:);
-        x = DecodeChromosome(chromosome, numberOfVariables, variableRange) ;
-        decodedPopulation(i,:)= x;
-        fitness(i) = EvaluateIndividual(x);
-        if ( fitness(i)> maximumFitness)
-            maximumFitness = fitness(i);
-            bestIndividualIndex = i;
-            xBest=x ;
+    surfaceFigureHandle= figure;
+    hold on;
+    set(surfaceFigureHandle,'DoubleBuffer','on');
+    delta=0.1;
+    limit = fix(2*variableRange/delta)+1 ;
+    [xValues, yValues] = meshgrid(-variableRange: delta:variableRange,-variableRange: delta:variableRange);
+    zValues= zeros(limit,limit);
+    for j = 1: limit
+        for k = 1: limit
+            zValues(j,k) = EvaluateIndividual([xValues(j,k) yValues(j,k)]);
         end
     end
+    surfl(xValues,yValues,zValues)
+    colormap gray;
+    shading interp;
+    view ([-7 -9 10]);
+    decodedPopulation = zeros(populationSize,numberOfVariables);
+    populationPlotHandle = plot3(decodedPopulation(:,1),decodedPopulation(:,2),fitness(:),'kp');
+    hold off;
+    drawnow;
+end
+%% INITIATE POPULATION
+population = InitializePopulation(populationSize, numberOfGenes) ;
 
+%% RUN GENERATIONS
+for iGeneration = 1: numberOfGenerations
     
+    %% FIND MAXIMUM FITNESS OF POPULATION
+
+    decodedPopulation = DecodePopulation(population, numberOfVariables, variableRange);
+    fitness = EvaluatePopulation(decodedPopulation, runparallel);
+    [maximumFitness, bestIndividualIndex] = max(fitness);
+    xBest = decodedPopulation(bestIndividualIndex,:);
+
+%   % Deprecated - to be deleted in the next iteration
+%     maximumFitness = 0.0;
+%     for i = 1: populationSize
+%         chromosome = population(i,:);
+%         x = DecodeChromosome(chromosome, numberOfVariables, variableRange) ;
+%         decodedPopulation(i,:)= x;
+%         fitness(i) = EvaluateIndividual(x);
+%         if ( fitness(i)> maximumFitness)
+%             maximumFitness = fitness(i);
+%             bestIndividualIndex = i;
+%             xBest=x ;
+%         end
+%     end
+
     % Print out
-    disp(maximumFitness);
-    disp(xBest); 
+    if verbose
+        fprintf('Maximum Fitness: %d\n',maximumFitness);
+        fprintf('Best Solution: %d\n',xBest); 
+    end
     
-    tempPopulation = population;
+    %% COPY POPULATION
+    newPopulation = population;
 
-    for i = 1:2:populationSize
+    %% NEW GENERATION
+    for i = 1:tournamentSize:populationSize
+        %% TOURNAMENT SELECTION
         i1 = TournamentSelect(fitness,tournamentSelectionParameter,tournamentSize);
         i2 = TournamentSelect(fitness,tournamentSelectionParameter,tournamentSize);
         chromosome1 = population(i1,:);
         chromosome2 = population(i2,:);
 
-        r= rand;
-        if ( r< crossoverProbability)
+        %% CROSS-OVER
+        r = rand;
+        if ( r < crossoverProbability)
             newChromosomePair = Cross(chromosome1, chromosome2);
-            tempPopulation(i,:) = newChromosomePair(1,:);
-            tempPopulation(i+1,:) = newChromosomePair(2,:);
+            newPopulation(i,:) = newChromosomePair(1,:);
+            newPopulation(i+1,:) = newChromosomePair(2,:);
         else
-            tempPopulation(i,:) = chromosome1;
-            tempPopulation(i+1,:) = chromosome2;
+            newPopulation(i,:) = chromosome1;
+            newPopulation(i+1,:) = chromosome2;
         end
     end
 
-    idx = rand(size(tempPopulation))<mutationProbability;                 % Index for Mutations
-    tempPopulation(idx) = tempPopulation(idx)*-1+1;                     % Bit Flip Occurs
+    %% MUTATE
+    newPopulation = Mutate(newPopulation, mutationProbability);
     
+    %% PRESERVATION OF PREVIOUS BEST SOLUTION
     bestChromosome = population(bestIndividualIndex,:);
-    tempPopulation = InsertBestIndividual(tempPopulation, bestChromosome, numberOfReplications);
-    population = tempPopulation;
+    newPopulation = InsertBestIndividual(newPopulation, bestChromosome, numberOfReplications);
+        
+    %% COPY THE NEW POPULATION ONTO CURRENT POPULATION
+    population = newPopulation;
     
-    plotvector= get(bestPlotHandle,'YData');
-    plotvector(iGeneration)= maximumFitness;
-    set(bestPlotHandle,'YData',plotvector);
-    set(textHandle,'String', sprintf('best: %4.3f',maximumFitness));
-    set(populationPlotHandle,'XData', decodedPopulation(:,1),'YData',decodedPopulation(:,2),'ZData', fitness(:));
-    drawnow;
+    %% PLOT CURRENT SITUATION
+    if draw_plots
+        plotvector = get(bestPlotHandle,'YData');
+        plotvector(iGeneration)= maximumFitness;
+        set(bestPlotHandle,'YData',plotvector);
+        set(textHandle,'String', sprintf('best: %4.3f',maximumFitness));
+        set(populationPlotHandle,'XData', decodedPopulation(:,1),'YData',decodedPopulation(:,2),'ZData', fitness(:));
+        drawnow;
+    end
 end
 
-disp(maximumFitness); 
-disp(xBest);
+% Print out
+fprintf('Maximum Fitness: %d\n',maximumFitness);
+fprintf('Best Solution: %d\n',xBest); 
 toc
