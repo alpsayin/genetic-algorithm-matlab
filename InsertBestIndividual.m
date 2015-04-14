fbf855e3c0e8bc21589de2a785f3f27aff31f6f5
Alp Sayin <alpsayin@gmail.com>
2015-04-14 10:37:30 +0100

    insert best individual onto random indexes (alternatively leave code for growing population)

--- InsertBestIndividual.m
+++ InsertBestIndividual.m
@@ -8,7 +8,19 @@
 
 function newPopulation = InsertBestIndividual( population, bestChromosome, nc)
 
+% Replace a random individual with the best chromosome
 newPopulation = population;
-for k = 1:nc
-    newPopulation(k,:) = bestChromosome;
-end
+randIndexes = ceil(rand(nc,1).*size(population,1));
+newPopulation(randIndexes,:) = repmat(bestChromosome,nc,1);
+
+return
+
+% Alternatively we can grow the population but generally people don't want growing population
+newPopulation = zeros(size(population,1)+nc, size(population,2));
+newPopulation(1:size(population,1)) = population;
+newPopulation(size(population,1)+1:end) = repmat(bestChromosome,nc,1);
+
+% Deprecated - to be deleted in the next iteration
+% for k = 1:nc
+%     newPopulation(k,:) = bestChromosome;
+% end
