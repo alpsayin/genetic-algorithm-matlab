478b2673d4fd1438a71953728f144b8c6ff3a184
Alp Sayin <alpsayin@gmail.com>
2015-04-14 10:22:08 +0100

    vectorize decoding process by using vector multiplication

--- DecodeChromosome.m
+++ DecodeChromosome.m
@@ -13,8 +13,12 @@ numberOfBits = size(chromosome,2) / numberOfVariables;
 
 for index = 1:numberOfVariables
     x(index) = 0.0 ;
-    for jj = 1:numberOfBits
-        x(index) = x(index) + chromosome(jj + (index-1)*numberOfBits )*(2^(-jj)) ;
-    end
+    geneRangeStart = (((index-1)*numberOfBits)+1);
+    geneRangeEnd = index*numberOfBits;
+    x(index) = sum(chromosome( geneRangeStart:geneRangeEnd ).*(2.^-(1:numberOfBits)));
+%     % Deprecated - to be deleted in the next iteration
+%     for jj = 1:numberOfBits
+%         x(index) = x(index) + chromosome(jj + (index-1)*numberOfBits )*(2^(-jj)) ;
+%     end
     x(index) = -variableRange + 2*variableRange*x(index)/(1-2^(-numberOfBits));
 end
